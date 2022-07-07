2a23293ea639e3af7cf6e36382a7b25dfc91e1af
hiroto22 <tochihirototo.nya.49@gmail.com>
2022-07-07 20:58:14 +0900

    heroku

--- todos/getTodoListWithUserID.go
+++ todos/getTodoListWithUserID.go
@@ -25,7 +25,7 @@ type TodoListWithUserID struct {
 }
 
 func GetTodoListWithUserId(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "applicaiton/json")
+	w.Header().Set("Content-Type", "*")
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	switch r.Method {
 	case "OPTIONS":
